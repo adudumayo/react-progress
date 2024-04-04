007bd47e48860a396b379aa49643ac03a3af52ac
Asimanye Dudumayo <adudumayo023@student.wethinkcode.co.za>
2024-04-04 17:52:50 +0200

    Learned about createRoot() and render()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello There!</h1>
+const logo = <h1>Task Arena</h1>
+const logoSpace = ReactDOM.createRoot(document.getElementById('logo'));
+logoSpace.render(logo);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+const slogan = document.getElementById('slogan');
+const sloganSpace = ReactDOM.createRoot(slogan);
+sloganSpace.render(<h2>Slogan Goes Here</h2>)
+
+const ans = <h3>5 + 5 is equal to {5 + 5}</h3>
+const calculationSpace = ReactDOM.createRoot(document.getElementById('calculation'));
+calculationSpace.render(ans);
+
+const list = (
+    <ul>
+        <li>React for front-end</li>
+        <li>Nodejs for back-end</li>
+        <li>MongoDB for Data Base</li>
+    </ul>
+);
+
+const projectsSpace = ReactDOM.createRoot(document.getElementById('projects'));
+projectsSpace.render(list);
