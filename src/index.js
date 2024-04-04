b534f0c063da2d37e1355c01d876f5b856222cd0
Asimanye Dudumayo <adudumayo023@student.wethinkcode.co.za>
2024-04-04 22:47:42 +0200

    Added the styling file and imported it in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './App.css';
 
 const logo = <h1>Task Arena</h1>
 const logoSpace = ReactDOM.createRoot(document.getElementById('logo'));
@@ -7,7 +8,7 @@ logoSpace.render(logo);
 
 const slogan = document.getElementById('slogan');
 const sloganSpace = ReactDOM.createRoot(slogan);
-sloganSpace.render(<h2>Slogan Goes Here</h2>)
+sloganSpace.render(<h2>Slogan Goes Here</h2>);
 
 const ans = <h3>5 + 5 is equal to {5 + 5}</h3>
 const calculationSpace = ReactDOM.createRoot(document.getElementById('calculation'));
